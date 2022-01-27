2e1d556bfb60661ef3d1389bd3e2b9775f2f94c9
Vu Nguuyen <vu.nguyen101@.com>
2022-01-27 12:47:02 +0200

    Fetch data from json-server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 function App() {
+const [notes, setNotes] = useState([])
+useEffect(async () => {
+  const response = await axios.get('http://localhost:5001/notes')
+  setNotes(response.data)
+}, [])
+
   return (
     <div>
       <h3>Imma LeadBar Link</h3>
+      {notes.map(note => <h5 key={note.id}>{note.content}</h5>)}
     </div>
   );
 }

2d6036aa346aeea89888e08ef1579e2ff4c1b120
Vu Nguuyen <vu.nguyen101@.com>
2022-01-27 14:01:45 +0200

    Add chip with avatar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import LeadBarLink from "./components/LeadBarLink";
-import axios from "axios";
 
 function App() {
-const [notes, setNotes] = useState([])
-useEffect(async () => {
-  const response = await axios.get('http://localhost:5001/notes')
-  setNotes(response.data)
-}, [])
 
   return (
     <div>
       <h3>Imma LeadBar Link</h3>
       <LeadBarLink />
-      {notes.map(note => <h5 key={note.id}>{note.content} {note.date}</h5>)}
     </div>
   );
 }
--- src/components/LeadBarLink.js
+++ src/components/LeadBarLink.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
 
 function LeadBarLink() {
   return (
-    <h4>I'm a link..</h4>
+    <a  href="https://www.serviceform.com/" style={{textDecoration: "none"}}>
+      <Stack direction="row" spacing={1}>
+        <Chip
+          avatar={<Avatar alt="Me" src="https://lh3.google.com/u/0/ogw/ADea4I7weFt0Z_euBRJDYIrQOD3bxFMC3USRqc-RXaRD=s83-c-mo" />}
+          label="Click me now to get 40% discount!!!"
+          color="primary"
+          clickable
+        />
+      </Stack>
+    </a>
+
   )
 }
 

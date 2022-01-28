da915e238d16cb8d9bc2459a0ac4aff2fa71ecac
Vu Nguuyen <vu.nguyen101@.com>
2022-01-28 14:55:57 +0200

    Style: modify link bg color

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import LeadBarLink from "./components/LeadBarLink";
 
+
 function App() {
-  const logo1 = 'https://lh3.google.com/u/0/ogw/ADea4I7weFt0Z_euBRJDYIrQOD3bxFMC3USRqc-RXaRD=s83-c-mo'
-  const logo2 = 'https://i.quotev.com/img/q/u/12/03/25/shika.jpg'
+  const logo1 = 'https://stripe-images.s3.us-west-1.amazonaws.com/works-with/0bf92fa8419e3237a6fb6eac9b1c225099f96741'
   const text1 = 'Click me for more discount!'
-  const text2 = 'Click me for free!'
   const url = 'https://www.serviceform.com/'
 
 
   return (
     <div>
-      <LeadBarLink url={url} text={text1} color="primary" logo={logo1}/>
-      <LeadBarLink url={url} text={text2} color="secondary" logo={logo2}/>
+      <LeadBarLink url={url} text={text1} logo={logo1}/>
     </div>
   );
 }
--- src/components/LeadBarLink.js
+++ src/components/LeadBarLink.js
@@ -2,22 +2,23 @@ import React from 'react';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
-import CelebrationIcon from '@mui/icons-material/Celebration';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import Link from '@mui/material/Link'
 
-function LeadBarLink({text, url, logo, btn, color}) {
+function LeadBarLink({text, url, logo}) {
+
   return (
     <Link  href={url} underline="none">
-      <Stack direction="row" spacing={1}>
+      <Stack direction="row" spacing={1} >
         <Chip
+          variant="filled"
+         color="info"
           // with avatar
           avatar={<Avatar alt="logo" src={logo} />}
           // with button
           onDelete={console.log}
           deleteIcon={<CheckCircleIcon />}
           label={text}
-          color={color}
           clickable
         />
       </Stack>
